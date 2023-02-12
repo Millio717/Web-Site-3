07b40bff4432cfca5fcf697f8a8daadaae5c353e
Sanshain <my-email@ya.ru>
2023-02-12 03:06:12 +0300

    burger is comming 2/3

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
 $(document).ready(function() {
-    function checkWidth() {
-        var windowWidth = $('body').innerWidth(),
-        elem = $(".about__wrapper-free");
-
-        if(windowWidth < 1145) {
-            elem.removeClass('.about__wrapper-free');
-        }
-        else {
-            elem.addClass('.about__wrapper-free');
-        }
-    }
-
-checkWidth();
-
-$(window).trigger('resize');
-
-})
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
